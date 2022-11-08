80d5ef13be52f3973563f1669a90134239097260
Jamie McDonald <jdamcd@gmail.com>
2022-11-08 15:23:39 -0500

    Remove deprecated packForXcode built task

--- shared/main/build.gradle.kts
+++ shared/main/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
@@ -9,13 +7,17 @@ version = AppVersion.name
 
 kotlin {
     android()
-    ios {
-        binaries {
-            framework {
-                baseName = "RunLogShared"
-            }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "RunLogShared"
         }
     }
+
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -43,10 +45,24 @@ kotlin {
                 implementation(Dependency.kotestAssert)
             }
         }
-        val iosMain by getting {
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
             dependencies {}
         }
-        val iosTest by getting {
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
             dependencies {
                 implementation(Dependency.kotestAssert)
             }
@@ -68,17 +84,3 @@ android {
     }
     namespace = "com.jdamcd.runlog.shared"
 }
-
-val packForXcode by tasks.creating(Sync::class) {
-    group = "build"
-    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
-    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
-    val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
-    val framework = kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
-    inputs.property("mode", mode)
-    dependsOn(framework.linkTask)
-    val targetDir = File(buildDir, "xcode-frameworks")
-    from({ framework.outputDirectory })
-    into(targetDir)
-}
-tasks.getByName("build").dependsOn(packForXcode)
