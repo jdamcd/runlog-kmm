a048a9aa6851b95e88272957eb496dcf71c4d0c6
Jamie McDonald <jdamcd@gmail.com>
2022-11-11 16:24:46 -0500

    Fix build config for arm64

--- shared/api/build.gradle.kts
+++ shared/api/build.gradle.kts
@@ -12,7 +12,9 @@ version = AppVersion.name
 
 kotlin {
     android()
-    ios()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -42,13 +44,26 @@ kotlin {
         val androidTest by getting {
             dependencies {}
         }
-        val iosMain by getting {
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
             dependencies {
                 implementation(Dependency.ktoriOS)
             }
         }
-        val iosTest by getting {
-            dependencies {}
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
