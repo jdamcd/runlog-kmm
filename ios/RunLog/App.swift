d1d2b346b7e9ec4ca1cac7bdb70d5ede3d08817d
Jamie McDonald <jdamcd@gmail.com>
2023-01-02 10:19:42 +0000

    Formatting

--- ios/RunLog/App.swift
+++ ios/RunLog/App.swift
@@ -3,14 +3,13 @@ import SwiftUI
 
 @main
 struct RunLogApp: App {
-    
     init() {
         SharedModuleKt.doInitKoin()
         userAuth = UserAuth()
     }
-    
+
     private var userAuth: UserAuth
-    
+
     var body: some Scene {
         WindowGroup {
             LaunchView().environmentObject(userAuth)
