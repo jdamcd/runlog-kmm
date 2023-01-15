d2c8172eb1b1fd70f7b704d53c3cbf8f02c2d1a0
Jamie McDonald <jdamcd@gmail.com>
2023-01-15 00:05:32 +0000

    Remove dead build config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,6 @@ pluginManagement {
         google()
         mavenCentral()
     }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.namespace == "com.android" || requested.id.name == "kotlin-android-extensions") {
-                useModule("com.android.tools.build:gradle:4.0.1")
-            }
-        }
-    }
 }
 rootProject.name = "RunLog"
 
