b384df1b88167708018b606cfe3a1e9486a31823
Jamie McDonald <jdamcd@gmail.com>
2024-11-22 11:01:25 -0500

    Bump iPhone simulator

--- shared/utils/src/iosMain/kotlin/com.jdamcd.runlog.shared.util/DebugConfig.kt
+++ shared/utils/src/iosMain/kotlin/com.jdamcd.runlog.shared.util/DebugConfig.kt
@@ -1,5 +1,8 @@
 package com.jdamcd.runlog.shared.util
 
+import kotlin.experimental.ExperimentalNativeApi
+
+@OptIn(ExperimentalNativeApi::class)
 actual object DebugConfig {
     actual val isDebug = Platform.isDebugBinary
 }
